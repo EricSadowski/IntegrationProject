f95701340d2095fd48d7bc9202a4d9b41655130f
RichardRosenberg <162045848+RichardRosenberg@users.noreply.github.com>
2024-04-17 11:45:43 -0400

    Implementing Ingrid FrontEnd
    added Ingrid's new design and the new login and registration components to match it. some basic designing still required.

--- musicFrontEnd/src/App.jsx
+++ musicFrontEnd/src/App.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "./components/Navbar";
-import Home from './pages/home';
-import Register from './pages/register';
-import Login from './pages/login';
-import Instruments from './pages/instruments';
-import '../src/styles/MainStyles.css' 
-import Lessons from './pages/lessons';
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Instruments from './components/Instruments/Instruments';
+import Title from './components/Title/Title';
+import About from './components/About/About';
+import Testimonials from './components/Testimonials/Testimonials';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import LoginForm from './components/LoginForm/LoginForm';
+import RegisterStudentForm from './components/RegisterForm/RegisterStudentForm';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  // Function to toggle login form visibility
+  const toggleLoginForm = () => {
+    setShowLogin(!showLogin);
+    setShowRegister(false); 
+  };
+
+  // Function to toggle registration form visibility
+  const toggleRegisterForm = () => {
+    setShowRegister(!showRegister);
+    setShowLogin(false); 
+  };
+
   return (
-    <Router>
-      <div >
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/instruments" element={<Instruments />} />
-          <Route path="/lesson" element={<Lessons />} />
-        </Routes>
+    document.title="Music Academy",
+    <div>
+      <Navbar toggleLoginForm={toggleLoginForm} toggleRegisterForm={toggleRegisterForm} />
+      <Hero toggleRegisterForm={toggleRegisterForm} />
+      <div className="container">
+        <Title title="Instruments" />
+        <Instruments />
+        <Title title="About Music Academy" />
+        <About />
+        <Title title="Testimonials" />
+        <Testimonials />
+        <Title title="Contact us" />
+        <Contact />
+        <Footer />
       </div>
-    </Router>
+      {showLogin && <LoginForm />}
+      {showRegister && <RegisterStudentForm />}
+    </div>
   );
 }
-
 
 export default App;
---
+++ musicFrontEnd/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './LoginForm.css';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [loggedInUsername, setLoggedInUsername] = useState('');
+  const [visible, setVisible] = useState(true);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8081/login/submit', {
+        username,
+        password
+      });
+      console.log('Login successful!');
+      setLoggedInUsername(username);
+      setSuccess('Login successful!');
+      setError('');
+    } catch (error) {
+      setError('Invalid username or password');
+      setSuccess('');
+    }
+  };
+
+  const handleClose = () => {
+    setVisible(false);
+  };
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className='page-container'>
+      <div className='register-form'>
+        <h1 className='login-header'>Login<span className="close-button" onClick={handleClose}>X</span></h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <br />
+          <div>
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <br />
+          <button type="submit">Login</button>
+          {error && <div className="error">{error}</div>}
+          {success && <div className="success">{success}</div>}
+        </form>        
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
